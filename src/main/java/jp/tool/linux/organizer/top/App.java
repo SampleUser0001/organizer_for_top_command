8a56741aeb8d65aa28d7f3ca3e4ad251e7d68256
Satoru Tanaka <ittimfn@gmail.com>
2020-07-08 15:21:18 +0900

    init commit

---
+++ src/main/java/jp/tool/linux/organizer/top/App.java
@@ -0,0 +1,13 @@
+package jp.tool.linux.organizer.top;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
